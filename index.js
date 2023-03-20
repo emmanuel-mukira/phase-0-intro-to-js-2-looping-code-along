0713118d1e9f2a9795b2db19bfa7699dde9c9821
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-03-20 14:28:12 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const name=["Guadalupe", "Ollie", "Aki"]
+function writeCards(name){
+    for (let i = 0;i<name.length; i++){
+    `Thank you, ${name[1]}, for the wonderful surprise gift!`;
+    }
+}
+function countDown(number){
+    while(number>-1){
+        console.log(number);
+        number--;
+    }
+}
